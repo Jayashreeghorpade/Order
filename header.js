95862c4507084871ef4dd21afa2a1ae552f040be
Jayashreeghorpade <jayaghorpade1993@gmail.com>
2023-07-23 10:58:07 +0530

    yes

---
+++ header.js
@@ -0,0 +1,17 @@
+const change =document.getElementById("box1");
+function header1(){
+    change.innerHTML="A B C D E F G H I"
+}
+function header2(){
+    change.innerHTML="A S D F G H J K L"
+}
+
+   
+
+   
+    
+    
+    
+    
+    
+    
